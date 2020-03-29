d7ce3ca7337c60d1f38f3c5f4bfea8ebc62106bb
Ray Alva <rayalva407@gmail.com>
2020-03-29 20:22:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect{|i| i.include?(cheese_types)
+  array.detect{|i| i.include?(cheese_types)}
 end
